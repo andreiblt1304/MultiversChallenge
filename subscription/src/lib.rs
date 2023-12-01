98bc692db9d26cf79064be8eaf8f4b28e788e148
Andrei Baltariu <andreibaltariu1304@gmail.com>
2023-12-01 14:08:35 +0200

    Fixed subscription lib.rs

--- subscription/src/lib.rs
+++ subscription/src/lib.rs
@@ -6,19 +6,20 @@ multiversx_sc::imports!();
 
 #[multiversx_sc::contract]
 pub trait Subscription:
+    payments::payments::PaymentsModule
 {
     #[init]
     fn init(
         &self,
-        price_query_address: ManagedAddress,
-        accepted_tokens: MultiValueEncoded<EgldOrEsdtTokenIdentifier>
+        price_query_address: ManagedAddress<Self::Api>,
+        accepted_tokens: MultiValueEncoded<Self::Api, EgldOrEsdtTokenIdentifier<Self::Api>>
     ) {
         require!(
             self.blockchain().is_smart_contract(&price_query_address),
             "Invalid price query address"
         );
 
-        // self.price_query_address().set(price_query_address);
-        // self.add_accepted_payments_tokens(accepted_tokens);
+        //self.price_query_address().set(price_query_address);
+        self.add_accepted_payment_tokens(accepted_tokens);
     }
 }
